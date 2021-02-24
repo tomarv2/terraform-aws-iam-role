3a8c71350c87dd00882b8a4022d6250864594701
tomarv2 <varuntomar2019@gmail.com>
2021-02-24 07:46:05 -0800

    moving version to seperate file

--- iam_role_instance/main.tf
+++ iam_role_instance/main.tf
@@ -6,13 +6,3 @@ locals {
       "Project", var.prjid
   )
 }
-
-provider "aws" {
-  region                      = var.aws_region
-  profile                     = var.profile_to_use
-  version                     = "~> 2.61"
-}
-
-terraform {
-  required_version            = ">= 0.12"
-}
