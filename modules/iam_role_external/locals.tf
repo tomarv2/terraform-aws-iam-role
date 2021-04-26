fb0b25edc3cab9023ba784b802f789456d995287
tomarv2 <varuntomar2019@gmail.com>
2021-04-26 11:53:32 -0700

    moving from map to tomap

--- modules/iam_role_external/locals.tf
+++ modules/iam_role_external/locals.tf
@@ -1,13 +1,13 @@
 locals {
   shared_tags = tomap(
     {
-      "Name" = "${var.teamid}-${var.prjid}",
-      "team" = var.teamid,
+      "Name"    = "${var.teamid}-${var.prjid}",
+      "team"    = var.teamid,
       "project" = var.prjid
     }
   )
 
   # https://github.com/Smartbrood/terraform-aws-ec2-iam-role/blob/master/README.md
-  role_name = var.name != null ? var.name : "${var.teamid}-${var.prjid}"
+  role_name          = var.name != null ? var.name : "${var.teamid}-${var.prjid}"
   assume_role_policy = coalesce(var.assume_role_policy, data.aws_iam_policy_document.policy_document.json)
 }
