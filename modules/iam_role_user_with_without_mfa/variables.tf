203b676075a8152576ee3414a0a391df8a9131f1
tomarv2 <varuntomar2019@gmail.com>
2021-06-25 22:08:27 -0700

    updating .gitignore file

--- modules/iam_role_user_with_without_mfa/variables.tf
+++ modules/iam_role_user_with_without_mfa/variables.tf
@@ -1,7 +1,6 @@
 variable "teamid" {
   description = "(Required) Name of the team/group e.g. devops, dataengineering. Should not be changed after running 'tf apply'"
   type        = string
-
 }
 
 variable "prjid" {
